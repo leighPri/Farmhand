979d5acb468c207ec0e3a833382eb7ceefb933c0
Leigh Prince <leighpri@gmail.com>
2017-12-27 14:10:15 -0500

    adds basic modal support and keyboard commands

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-const editGoal = (id) => {
+const goalContainer = document.getElementById('goal-container');
+const modalOverlay = document.getElementById('modal-overlay');
+const modalContent = document.getElementById('modal-content');
+
+const modals = {
+  help: `
+    <div class="modal-title">Controls</div>
+    <div class="modal-body">
+      <ul>
+        <li>Click buttons to increment</li>
+        <li>Click anywhere on goal to focus</li>
+        <li>Space bar increases focused goal</li>
+        <li>Alt/option + space decreases focused goal</li>
+        <li>Hold shift to increment by 10</li>
+      </ul>
+    </div>
+  `
+};
+
+let goalHash = {
+  alsdjf4308fjsjdf: { title: 'First Goal', image: '', current: 0, needed: 100 },
+  alsdjf4308fjsjddf: { title: 'Second Goal', image: '', current: 0, needed: 100 },
+  alsdjf43018fjsjdf: { title: 'Third Goal', image: '', current: 0, needed: 100 },
+  alsdjf4308dsfjsjdf: { title: 'Fourth Goal', image: '', current: 0, needed: 100 },
+  alsdjf4308wfjsjdf: { title: 'Fifth Goal', image: '', current: 0, needed: 100 },
+  alsdjf4308dfjsjdf: { title: 'Sixth Goal', image: '', current: 0, needed: 100 }
+};
+
+let focusedGoal = '';
+
+let goalToEdit = '';
+
+let goalToDelete = '';
+
+const editGoal = (e, id) => {
+  e.stopPropagation();
+  goalToEdit = id;
   console.log('you clicked edit on ' + id);
 };
 
-const deleteGoal = (id) => {
+const deleteGoal = (e, id) => {
+  e.stopPropagation();
+  goalToDelete = id;
   console.log('you clicked delete on ' + id);
+};
+
+const focusGoal = (id) => {
+  focusedGoal = id;
 };
 
 const changeAmount = (e, increase, id) => {
@@ -14,5 +56,15 @@ const changeAmount = (e, increase, id) => {
     goalHash[id].current -= increment;
     if (goalHash[id].current < 0) goalHash[id].current = 0;
   }
-  renderGoals();
+  renderGoals(goalHash, goalContainer);
 };
+
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 32 && focusedGoal.length) {
+    changeAmount(e, !e.altKey, focusedGoal);
+  } else if (e.keyCode === 27) {
+    hideModal();
+  }
+});
+
+renderGoals(goalHash);
--- render.js
+++ render.js
@@ -1,14 +1,12 @@
-let goalContainer = document.getElementById('goal-container');
-
-// creation funciton needs to generate a random key based on the title
-let goalHash = {
-  alsdjf4308fjsjdf: { title: 'First Goal', image: '', current: 0, needed: 100 },
-  alsdjf4308fjsjddf: { title: 'Second Goal', image: '', current: 0, needed: 100 },
-  alsdjf43018fjsjdf: { title: 'Third Goal', image: '', current: 0, needed: 100 },
-  alsdjf4308dsfjsjdf: { title: 'Fourth Goal', image: '', current: 0, needed: 100 },
-  alsdjf4308wfjsjdf: { title: 'Fifth Goal', image: '', current: 0, needed: 100 },
-  alsdjf4308dfjsjdf: { title: 'Sixth Goal', image: '', current: 0, needed: 100 }
+const showModal = (modal) => {
+  modalOverlay.style.display = 'block';
+  modalContent.innerHTML = modals[modal];
 };
+
+const hideModal = () => {
+  modalOverlay.style.display = 'none';
+  modalContent.innerHTML = '';
+}
 
 const createUniqueKey = (objToCheck) => {
   let newKey = '_' + Math.random().toString(36).substr(2, 9);
@@ -18,15 +16,15 @@ const createUniqueKey = (objToCheck) => {
   return newKey;
 };
 
-const createGoal = (title, image, current, needed) => {
+const createGoal = (goalHash, title, image, current, needed) => {
   let id = '_' + new Date().getTime();
   console.log(id);
   goalHash[id] = { title, image, current, needed };
   console.log(goalHash);
-  renderGoals(goalContainer);
+  renderGoals(goalHash);
 };
 
-const renderGoals = () => {
+const renderGoals = (goalHash) => {
   let goalHtml = '';
   for (const id in goalHash) {
     goalHtml += createGoalCard(id, goalHash[id]);
@@ -37,22 +35,17 @@ const renderGoals = () => {
 const createGoalCard = (id, goalObj) => {
   const { title, image, current, needed } = goalObj;
   return `
-    <div class="goal-card" id="${id}">
+    <div class="goal-card" id="${id}" onClick="focusGoal('${id}')">
       <div class="goal-title">${title}</div>
-      <div class="goal-edit-button goal-button" onClick="editGoal('${id}')">EDIT</div>
+      <div class="goal-edit-button goal-button" onClick="editGoal(event, '${id}')">EDIT</div>
       <div class="goal-amount">
         <div class="minus-button" onClick="changeAmount(event, false, '${id}')">-</div>
         <div class="current-amount">${current}</div> / 
         <div class="needed-amount">${needed}</div>
         <div class="plus-button" onClick="changeAmount(event, true, '${id}')">+</div>
       </div>
-      <div class="goal-delete-button goal-button" onClick="deleteGoal('${id}')">DELETE</div>
+      <div class="goal-delete-button goal-button" onClick="deleteGoal(event, '${id}')">DELETE</div>
     </div>
   `;
 };
 
-renderGoals();
-
-// setTimeout(() => {
-//   createGoal('Seventh Goal', '', 0, 100);
-// }, 5000);
